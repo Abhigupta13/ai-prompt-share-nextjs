a5d6ae5b14d1556b119fc26f2274fbaaf501dcb0
Abhishek Kumar Gupta <100848632+Abhigupta13@users.noreply.github.com>
2024-05-15 01:15:42 +0530

    fixed some issues

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,27 +1,12 @@
-import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
 
-export const GET = async (request) => {
+export const GET = async(request)=>{
     try {
         await connectToDB();
-        const prompts = await Prompt.find().populate({
-          path: "creator"
-        });
-
-        const response = new Response(JSON.stringify(prompts), {
-          status: 200,
-        });
-
-        // Add a unique identifier to the URL to force a cache-busting reload
-        const url = new URL(req.url);
-        url.searchParams.set("t", Date.now());
-        response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
-        response.headers.set("Pragma", "no-cache");
-        response.headers.set("Expires", "0");
-        response.headers.set("Location", url.toString());
-
-        return response;
+        const prompts = await Prompt.find({}).populate('creator');
+        return new Response(JSON.stringify(prompts),{status:200});
     } catch (error) {
-        return new Response("Failed to fetch all prompts", { status: 500 })
+        return new Response("Failed to fetch all prompts",{status:500});
     }
-} 
+}
